4bf2a21607e9dbb0ffb6df95902e2e316e8f806d
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-08-04 13:26:27 +0100

    place online status into user control

---
+++ videoLibrary/UserControl/DisplayMachinesStatus.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace videoLibrary.UserControl
+{
+    /// <summary>
+    /// Interaction logic for DisplayMachinesStatus.xaml
+    /// </summary>
+    public partial class DisplayMachinesStatus
+    {
+        /// <summary>
+        /// Dependency Property used to back the <see cref="IsPiOnline"/> property
+        /// </summary>
+        public static readonly DependencyProperty IsPiOnlineProperty =
+            DependencyProperty.Register("IsPiOnline", 
+                typeof(bool), 
+                typeof(DisplayMachinesStatus), 
+                new PropertyMetadata(false));
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public DisplayMachinesStatus()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Boolean represents if the pi is online or not
+        /// </summary>
+        public bool IsPiOnline
+        {
+            get { return (bool)GetValue(IsPiOnlineProperty); }
+            set { SetValue(IsPiOnlineProperty, value); }
+        }
+    }
+}
