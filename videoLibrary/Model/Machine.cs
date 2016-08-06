1a828df9427544dbddb42579e783a78dcc4355a6
Sam Greaves <samgreaves0@gmail.com>
2016-08-06 15:49:05 +0100

    add machine class

---
+++ videoLibrary/Model/Machine.cs
@@ -0,0 +1,40 @@
+﻿
+namespace videoLibrary.Model
+{
+    public class Machine : BaseModel
+    {
+        private string _ip;
+        private string _machineName;
+        private bool _isOnline;
+
+        public string Ip
+        {
+            get { return _ip; }
+            set
+            {
+                _ip = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string MachineName
+        {
+            get { return _machineName; }
+            set
+            {
+                _machineName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsOnline
+        {
+            get { return _isOnline; }
+            set
+            {
+                _isOnline = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
